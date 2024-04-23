50922b113d2013f21f43d7aaad1f1c6af28bd1ee
Aleksandr <genkonst5@gmail.com>
2024-04-23 18:08:58 +0300

    fix dbinit

--- dump/initAtonData.sql
+++ dump/initAtonData.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 16.1
 -- Dumped by pg_dump version 16.1
 
--- Started on 2024-04-23 16:27:28
+-- Started on 2024-04-23 17:59:02
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,23 +17,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- TOC entry 4 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
-CREATE SCHEMA public;
-
-
---
--- TOC entry 4841 (class 0 OID 0)
--- Dependencies: 4
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
 
 SET default_tablespace = '';
 
@@ -206,9 +189,8 @@ ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_un UNIQUE (login);
 
 
--- Completed on 2024-04-23 16:27:29
+-- Completed on 2024-04-23 17:59:02
 
 --
 -- PostgreSQL database dump complete
 --
-
